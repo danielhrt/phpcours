c0d43db1acdeb743bdc7091450e1135efc4d9412
danielhrt <73801890+danielhrt@users.noreply.github.com>
2020-12-09 15:54:07 +0100

    fin exercice 3

---
+++ exercice 3/index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>excercice 3</title>
+    <style type=text/css>
+        p{
+            color: red;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $admin = false;
+    if($admin == true){
+        echo "bienvenue viens <a href=#>ici</a>";
+    }
+    else{
+        echo "<p>erreur page réservé aux admins";
+    }
+    ?>
+</body>
+</html>
