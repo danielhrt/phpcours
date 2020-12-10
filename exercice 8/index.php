ae12ca07fb50568a8735a8c36b439672528d98d0
danielhrt <73801890+danielhrt@users.noreply.github.com>
2020-12-10 14:45:46 +0100

    exercice 8

---
+++ exercice 8/index.php
@@ -0,0 +1,46 @@
+<?php
+    $userInfos[
+        [
+            'name' => 'Dan',
+            'age' => '26',
+            'city' => 'Paris'
+        ],
+        [
+            'name' => 'Jean',
+            'age' => '17',
+            'city' => 'Lille'
+        ],
+        [
+            'name' => 'Fred',
+            'age' => '38',
+            'city' => 'Tours'
+        ],
+        [
+            'name' => 'Romain',
+            'age' => '41',
+            'city' => 'Nantes'
+        ],
+        [
+            'name' => 'Lou',
+            'age' => '22',
+            'city' => 'Lyon'
+        ],
+    ];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 8</title>
+</head>
+<body>
+    <?php
+        foreach($userInfos as $array){
+            echo '<pre>';
+            print_r($userInfos);
+            echo '</pre>';
+        }
+    ?>
+</body>
+</html>
