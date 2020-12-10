3dd7f442df5b2c2b2f031617e4b51aeab80499db
danielhrt <73801890+danielhrt@users.noreply.github.com>
2020-12-10 10:28:24 +0100

    fin ecercice 6

---
+++ exercice 6/index.php
@@ -0,0 +1,20 @@
+<?php
+    $prénom = ['Bob', 'Anthony', 'Jean', 'Max', 'Dan', 'Alex', 'Tom', 'Ben', 'Lea', 'Gwen'];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 6</title>
+</head>
+<body> 
+    <ul>
+    <?php
+        for($i = 0; $i < 10; $i++){
+            echo '<li>' . $prénom[$i] . '</li>';
+        };
+    ?>
+    </ul>
+</body>
+</html>
