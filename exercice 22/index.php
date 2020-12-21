cb8de42e329d4c9ce008610014b9b66efed30e55
danielhrt <73801890+danielhrt@users.noreply.github.com>
2020-12-21 15:26:25 +0100

    fin exercice 22

--- exercice 22/index.php
+++ exercice 22/index.php
@@ -1,13 +1,35 @@
 <?php
         $fichier = fopen('compteur.txt', 'r+');
         $compteur = fgets($fichier);
-        $compteur += 1;
+        $compteur++;
         fseek($fichier, 0);
         fwrite($fichier, $compteur);
         fclose($fichier);
         ?>
-
-     	<?php
-			echo
-              '<p class="visiteur">Nombre de vue de cette page: '. $compteur.'</p>';
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 22</title>
+    <style>
+        .visiteur{
+            color: blue;
+            border: 2px blue solid;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            background-color: white;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1 style="text-align: center; color:red;">Bienvenue !</h1>
+    <img src="img.jpg" alt="pikachu" style="width: 100%;">
+    <?php
+			echo '<p class="visiteur">Nombre de vue de cette page: ' . $compteur. '</p>';
 		?>
+</body>
+</html>
